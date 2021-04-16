0a32991d8f9005a9b5841b6c3853924e6344d212
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-16 05:25:54 -0400

    Update Jest+Mongoose tests and refactor
    # Conflicts:
    #	server/src/server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,7 @@
 const http = require('http');
-const mongoose = require('mongoose');
 
 const app = require('./app');
-
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
@@ -12,15 +11,8 @@ const MONGO_URL = 'mongodb+srv://drvinhhoang:Hoangvinh93@nasacluster.kx8xc.mongo
 
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
+  await mongoConnect();
   await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   
