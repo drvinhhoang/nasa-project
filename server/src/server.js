3a7d8acebb1fe1736b9a0614366cde62bb3111f3
Vinh H <vinh.hoang@bepos.io>
2024-10-01 20:42:25 +0700

    Connect to Mongo cluster
    # Conflicts:
    #	server/package-lock.json
    #	server/package.json

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,21 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+// Update below to match your own MongoDB connection string.
+const MONGO_URL = 'mongodb+srv://drvinhhoang:Hoangvinh93@nasacluster.kx8xc.mongodb.net/?retryWrites=true&w=majority&appName=NASAClustery';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   
   server.listen(PORT, () => {
